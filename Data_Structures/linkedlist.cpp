2fbb980fe00f36f7f823d6d951fe56377770b5ed
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2021-03-08 10:46:26 -0800

    added linkled list

---
+++ Data_Structures/linkedlist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    
+
+};
+
+void printList(int arr[], int len){
+    for(int i = 0; i < len; i++){
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+
+
+}
+
+
+
+int main(){
+    Node n1, n2, n3, n4, n5;
+    
+
+
+}
